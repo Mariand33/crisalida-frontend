827ec408e6fb4e2a1a5f882cd413460abafaba60
Mariand33 <invictussdigital@gmail.com>
2026-06-15 15:55:30 -0300

    Update data.js

--- crisalida-frontend/data.js
+++ crisalida-frontend/data.js
@@ -4,6 +4,7 @@
 window.usuariosData = [
   { email: "sabrina@crisalida.com", password: "crsld2026#", nombre: "Sabrina" },
   { email: "karina@crisalida.com", password: "creador26", nombre: "Karina" },
+   { email: "maria@crisalida.com", password: "yosoy", nombre: "Mary" }
   { email: "rosy@crisalida.com", password: "creador33", nombre: "Rosy" }
    { email: "maria@crisalida.com", password: "yosoy", nombre: "Mary" }
   
